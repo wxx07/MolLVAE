8bcecfd8b525f348a6e78db67880fb71b1cd167b
clownjiahui <787492091@qq.com>
2020-05-05 18:14:58 +0800

    LSTM_encoder

--- code/model/encoders/LSTM_encoder.py
+++ code/model/encoders/LSTM_encoder.py
@@ -3,41 +3,50 @@ import torch.nn as nn
 import numpy as np
 from torch.nn.utils.rnn import pack_padded_sequence,pad_packed_sequence
 
-
 class LSTM_encoder(torch.nn.Module):
-    def __init__(self,input_size,hidden_size,num_layers,vocab_size,batch_size,
-                 bidirectional=False):
+    def __init__(self,input_size,hidden_size,num_layers,vocab_size,batch_size,sorted_seq=False,
+                 bidirectional=False,):
         '''
         :param input_size:  Input size of LSTM
         :param hidden_size: Hidden size of LSTM
         :param num_layers: Number of recurrent layers.
-        :param vocab_size: Vocabulary size of embedding size
+        :param vocab_size: Vocabulary size of embedding
         :param batch_size: Batch size
+        :param sorted_seq: If the input_seq has been sorted,set this param to True and give
+                           the seq and lengths when forward function is called. Default: False
         :param bidirectional: If True, becomes a bidirectional LSTM. Default: False
         '''
         super(LSTM_encoder,self).__init__()
-        self.hidden_size = hidden_size
         self.batch_size = batch_size
+        self.sorted_seq = sorted_seq
         self.embedding = nn.Embedding(vocab_size,input_size)
         self.lstm = nn.LSTM(input_size,hidden_size,num_layers,bidirectional = bidirectional)
 
-    def forward(self,seq):
-        # sort input sqeuence
-        lengths = self.get_length(seq)
-        _, idx_sort = torch.sort(torch.Tensor(lengths), dim=0, descending=True)
-        _, idx_unsort = torch.sort(idx_sort, dim=0)
-        seq_sort = seq.index_select(0, idx_sort)
-        lengths_sort = lengths.index_select(0, idx_sort)
-        # forward
-        embeds = self.embedding(seq_sort)
-        seq_sort = pack_padded_sequence(embeds,lengths=list(lengths_sort),batch_first=True)
-        output,(h_n,c_n) = self.lstm(seq_sort)
-        output,_ = pad_packed_sequence(output,batch_first=True)
-        h_n = h_n[-(1+int(self.lstm.bidirectional)):]
-        h_n = torch.cat(h_n.split(1), dim=-1).squeeze(0)
-        #unsort output and h_n
-        output = output.index_select(0, idx_unsort)
-        h = h_n.index_select(0, idx_unsort)
+    def forward(self,seq,lengths=None):
+        if self.sorted_seq:
+            embeds = self.embedding(seq)
+            seq = pack_padded_sequence(embeds, lengths=list(lengths), batch_first=True)
+            output, (h_n, c_n) = self.lstm(seq)
+            output, _ = pad_packed_sequence(output, batch_first=True)
+            h_n = h_n[-(1 + int(self.lstm.bidirectional)):]
+            h = torch.cat(h_n.split(1), dim=-1).squeeze(0)
+        else:
+            # sort input sqeuence
+            lengths = self.get_length(seq)
+            _, idx_sort = torch.sort(torch.Tensor(lengths), dim=0, descending=True)
+            _, idx_unsort = torch.sort(idx_sort, dim=0)
+            seq_sort = seq.index_select(0, idx_sort)
+            lengths_sort = lengths.index_select(0, idx_sort)
+            # forward
+            embeds = self.embedding(seq_sort)
+            seq_sort = pack_padded_sequence(embeds,lengths=list(lengths_sort),batch_first=True)
+            output,(h_n,c_n) = self.lstm(seq_sort)
+            output,_ = pad_packed_sequence(output,batch_first=True)
+            h_n = h_n[-(1+int(self.lstm.bidirectional)):]
+            h_n = torch.cat(h_n.split(1), dim=-1).squeeze(0)
+            #unsort output and h_n
+            output = output.index_select(0, idx_unsort)
+            h = h_n.index_select(0, idx_unsort)
         return output,h
 
     def get_length(self,x):
