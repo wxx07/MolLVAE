fc9a623e0294592b9e9f83270a7f2377fb58bd54
clownjiahui <787492091@qq.com>
2020-05-08 15:33:01 +0800

    LSTM_encoder and model update

--- code/model/encoders/LSTM_encoder.py
+++ code/model/encoders/LSTM_encoder.py
@@ -1,53 +1,41 @@
 import torch
 import torch.nn as nn
 import numpy as np
-from torch.nn.utils.rnn import pack_padded_sequence,pad_packed_sequence,pad_sequence
+from torch.nn.utils.rnn import pack_padded_sequence,pad_packed_sequence
 
 class LSTM_encoder(torch.nn.Module):
     def __init__(self,embed_layer,vocab,config):
-        '''
-        :param embed_size:  Input size of LSTM
-        :param embed_layer: embedding layer
-        :param hidden_size: Hidden size of LSTM
-        :param num_layers: Number of recurrent layers.
-        :param vocab: Vocabulary of embedding
-        :param dropout: dropout layer follows lstm layer
-        :param sorted_seq: If the input_seq has been sorted,set this param to True and give
-                           the seq and lengths when forward function is called. Default: False
-        :param bidirectional: If True, becomes a bidirectional LSTM. Default: False
-        '''
         super(LSTM_encoder,self).__init__()
         self.sorted_seq = config.enc_sorted_seq
         self.pad = vocab.pad
         self.embedding = embed_layer
-        self.lstm = nn.LSTM(config.embed_size,config.enc_hidden_size,
-                            config.enc_num_layers,bidirectional = config.enc_bidirectional,
+        self.lstm = nn.LSTM(config.embed_size,config.enc_hidden_size,batch_first=True,
+                            num_layers=config.enc_num_layers,bidirectional = config.enc_bidirectional,
                             dropout= config.enc_dropout if config.enc_num_layers > 1 else 0)
 
-    def forward(self,seq,lengths=None):
+    def forward(self,batch):
         if self.sorted_seq:
-            embeds = [self.embedding(i) for i in seq]
-            seq = pad_sequence(embeds, batch_first=True, padding_value=self.pad)
-            seq = pack_padded_sequence(seq, lengths=list(lengths), batch_first=True,)
+            seq,lengths = batch
+            embeds = self.embedding(seq) # batch_size * padded_len * embed_size
+            seq = pack_padded_sequence(embeds, lengths=list(lengths), batch_first=True)
             output, (h_n, c_n) = self.lstm(seq)
             output, _ = pad_packed_sequence(output,batch_first=True)
             h_n = h_n[-(1 + int(self.lstm.bidirectional)):]
-            h = torch.cat(h_n.split(1), dim=-1).squeeze(0)
+            h = torch.cat(h_n.split(1), dim=-1).squeeze(0) # batch_size * enc_hidden_size * i (if enc_bidirectional = True,i=2;else i=1)
         else:
             # sort input sqeuence
-            lengths = self.get_length(seq)
+            lengths = self.get_length(batch)
             _, idx_sort = torch.sort(torch.Tensor(lengths), dim=0, descending=True)
             _, idx_unsort = torch.sort(idx_sort, dim=0)
-            seq_sort = seq.index_select(0, idx_sort)
+            seq_sort = batch.index_select(0, idx_sort)
             lengths_sort = lengths.index_select(0, idx_sort)
             # forward
-            embeds = [self.embedding(i) for i in seq_sort]
-            seq_sort = pad_sequence(embeds, batch_first=True, padding_value=self.pad)
-            seq_sort = pack_padded_sequence(seq_sort,lengths=list(lengths_sort),batch_first=True)
+            embeds = self.embedding(seq_sort) # batch_size * padded_len * embed_size
+            seq_sort = pack_padded_sequence(embeds,lengths=list(lengths_sort),batch_first=True)
             output,(h_n,c_n) = self.lstm(seq_sort)
             output,_ = pad_packed_sequence(output,batch_first=True)
             h_n = h_n[-(1+int(self.lstm.bidirectional)):]
-            h_n = torch.cat(h_n.split(1), dim=-1).squeeze(0)
+            h_n = torch.cat(h_n.split(1), dim=-1).squeeze(0) # batch_size * enc_hidden_size * i (if enc_bidirectional = True,i=2;else i=1)
             #unsort output and h_n
             output = output.index_select(0, idx_unsort)
             h = h_n.index_select(0, idx_unsort)
--- code/model/model.py
+++ code/model/model.py
@@ -3,55 +3,60 @@ import torch.nn as nn
 import torch.nn.functional as F
 from code.model.decoders.LSTM_decoder import LSTM_decoder
 from code.model.encoders.LSTM_encoder import LSTM_encoder
-from code.dataset import DatasetSplit
 
 
 class LVAE(torch.nn.Module):
     def __init__(self,vocab,config):
-        '''
-        :param encoder_param: encoder type and encoder param
-        :param decoder_param: decoder type and decoder param
-        :param vocab: Vocabulary of embedding
-        :param d_size: list contain d_size of LVAE. e.g. [512,256,128,64,32]
-        :param z_size: list contain z_size of  LVAE e.g. [64,32,16,8,4]
-        :param z2z_layer_size: list contain z2z layer size of LVAE e.g. [8,16,32,64]
-        :param batch_size: batch size
-        '''
         super(LVAE,self).__init__()
         for ss in ('bos', 'eos', 'unk', 'pad'):
             setattr(self, ss, getattr(vocab, ss))
-        self.d_size = config.LVAE_d_size
-        self.z_size = config.LVAE_z_size
-        self.z2z_layer_size = config.LVAE_z2z_layer_size
-        self.batch_size = config.LVAE_batch_size
-        self.z_reverse_size = list(reversed(config.LVAE_z_size))
+        self.d_size = config.ladder_d_size
+        self.z_size = config.ladder_z_size
+        self.z2z_layer_size = config.ladder_z2z_layer_size
+        self.z_reverse_size = list(reversed(config.ladder_z_size))
         self.vocab = vocab
+        # Word embeddings layer
         self.embedding = nn.Embedding(len(self.vocab.i2c),config.embed_size,self.vocab.pad)
+
+        # Encoder
         if config.enc_type == 'lstm':
             self.encoder = LSTM_encoder(self.embedding,self.vocab,config)
-            self.LVAE_input_size = config.enc_hidden_size * (1 + int(config.enc_bidirectional))
+            self.ladder_input_size = config.enc_hidden_size * (1 + int(config.enc_bidirectional))
         else :
             raise ValueError(
                 "Invalid encoder_type"
             )
 
+        # Decoder
         if config.dec_type == 'lstm':
-            self.decoder = LSTM_decoder(self.vocab,self.embedding,config,config.LVAE_z_size[0])
+            self.decoder = LSTM_decoder(self.vocab,self.embedding,config,config.ladder_z_size[0])
         else:
             raise ValueError(
                 "Invalid decoder_type"
             )
+
+        #ladder
         self.top_down_layers = []
         self.bottom_up_layers = []
+
+        #get a list contain bottom up layers,which used to generate z_mu_q_d and z_var_q_d
         for i in range(len(self.d_size)):
             if i == 0:
-                self.bottom_up_layers.append(MLP(in_size=self.LVAE_input_size,layer_size=self.d_size[i],out_size=self.z_size[i]))
+                self.bottom_up_layers.append(MLP(in_size=self.ladder_input_size,layer_size=self.d_size[i],out_size=self.z_size[i]))
             else:
                 self.bottom_up_layers.append(MLP(in_size=self.d_size[i-1],layer_size=self.d_size[i],out_size=self.z_size[i]))
+
+        #get a list contain top down layers,which used to generate z_mu_p and z_var_p
         for i in range(len(self.z_reverse_size)-1):
             self.top_down_layers.append(MLP(in_size=self.z_reverse_size[i],layer_size=self.z2z_layer_size[i],out_size=self.z_reverse_size[i+1]))
 
     def bottom_up(self,input):
+        '''
+        Do the bottom up step and get z_mu_q_d and z_var_q_d
+
+        :param: input of ladder part,size=(batch_size * ladder_input_size)
+        :return:two lists: z_mu_q_d ,z_var_q_d
+        '''
         z_mu_q_d = []
         z_var_q_d = []
         for i in range(len(self.d_size)):
@@ -66,6 +71,14 @@ class LVAE(torch.nn.Module):
         return z_mu_q_d, z_var_q_d
 
     def top_down(self, z_sample, z_mu_p, z_var_p):
+        '''
+        Do top down step and get z_mu_p,z_var_p, also return samples of each level z
+
+        :param z_sample: only have the samples of top z
+        :param z_mu_p: only have the z_mu_p of top z (mu = 0)
+        :param z_var_p: only have the z_var_p of top z (var = 1)
+        :return three list :z_mu_p,z_var_p,z_sample
+        '''
         for i in range(len(self.z_reverse_size) - 1):
             _, mu_p, var_p = self.top_down_layers[i](z_sample[i])
             z_sample.append(self.sample_z(mu_p, var_p))
@@ -74,56 +87,128 @@ class LVAE(torch.nn.Module):
         return list(reversed(z_mu_p)), list(reversed(z_var_p)), list(reversed(z_sample))
 
     def sample_z(self, mu, var):
+        '''
+        Sampling z ~ p(z)= N(mu,var)
+        :return: sample of z
+        '''
         stddev = var ** 0.5
         out = mu + stddev * torch.randn(mu.size())
         return out
 
-    def Gaussian_up_date(self, mu_d, var_d, mu_up, var_up):
-        x = torch.pow(var_d, -1)
-        y = torch.pow(var_up, -1)
+    def Gaussian_update(self, mu_q_d, var_q_d, mu_p, var_p):
+        '''
+        Combine mu_q_d,var_q_d and mu_p,var_p to generate mu_q,var_q
+        :return two tensor: mu_q,var_q
+        '''
+        x = torch.pow(var_q_d, -1)
+        y = torch.pow(var_p, -1)
         sigma = torch.pow(torch.add(x, y), -1)
         var = torch.pow(sigma, 2)
-        mu = torch.add(mu_d*x, mu_up*y) * sigma
+        mu = torch.add(mu_q_d*x, mu_p*y) * sigma
         return mu, var
 
     def KL_loss(self,q_mu,q_var,p_mu,p_var):
         kl = 0.5*(torch.log(p_var)-torch.log(q_var) + q_var/p_var + torch.pow(torch.add(q_mu,-p_mu),2)/p_var -1)
         return kl.sum(1).mean()
 
-    def forward_encoder(self,seq,lengths):
-        _,input = self.encoder(seq,lengths)
+    def forward_latent(self,input):
+        # initialize required lists
         z_mu_p = []
         z_var_p = []
         z_sample = []
         z_mu_q = []
         z_var_q = []
         kl_loss = 0
+
+        #do bottom up step and get z_mu_q_d, z_var_q_d
         z_mu_q_d, z_var_q_d = self.bottom_up(input)
+
+        #add mu_p,var_p and samples of top z to list
         z_mu_p.append(torch.zeros(z_mu_q_d[-1].size()))
         z_var_p.append(torch.ones(z_var_q_d[-1].size()))
         z_sample.append(self.sample_z(z_mu_q_d[-1], z_var_q_d[-1]))
+
+        #do top down step and get z_mu_p, z_var_p,z_sample
         z_mu_p, z_var_p, z_sample = self.top_down(z_sample, z_mu_p, z_var_p)
+
+        #combine z_mu_q_d, z_var_q_d, z_mu_p, z_var_p to generate z_mu_q and z_var_q
         for i in range(len(self.z_size)):
-            mu, var = self.Gaussian_up_date(z_mu_q_d[i], z_var_q_d[i], z_mu_p[i], z_var_p[i])
+            mu, var = self.Gaussian_update(z_mu_q_d[i], z_var_q_d[i], z_mu_p[i], z_var_p[i])
             z_mu_q.append(mu)
             z_var_q.append(var)
+
+        #calculate KL_loss
         for i in range(len(self.z_size)):
             kl_loss = kl_loss + self.KL_loss(z_mu_q[i], z_var_q[i], z_mu_p[i], z_var_p[i])
         return z_sample[0],kl_loss
 
     def forward(self, batch):
-        seq,lengths = batch
-        z,KL_loss= self.forward_encoder(seq,lengths)
+        _,h = self.encoder(batch)
+        z,KL_loss = self.forward_latent(h)
         recon_loss = self.decoder(batch,z)
         return KL_loss,recon_loss
 
-    # def sample(self,n_batch, max_len=100, z=None, temp=1.0):
-    #     '''
-    #     :param n_batch: number of mol to generate
-    #     :param max_len: max len of samples
-    #     :param z: (n_batch, d_z) of floats, latent vector z or None
-    #     :param temp: temperature of softmax
-    #     '''
+    def tensor2string(self, tensor):
+        ids = tensor.tolist()
+        string = self.vocab.ids2string(ids, rem_bos=True, rem_eos=True)
+        return string
+
+    def sample(self,n_batch,max_len=100,temp=1.0,z=None):
+        '''
+        Generating n_batch samples
+
+        :param n_batch: number of sentences to generate
+        :param max_len: max len of samples
+        :param temp: temperature of softmax
+        :param z: (n_batch, ladder_z_size[-1]) of floats, vector of top z or None
+        :return: list of tensors of strings, samples sequence x
+        '''
+        with torch.no_grad():
+            # get samples of  z
+            z_sample = []
+            z_mu_p = []
+            z_var_p =[]
+            z_mu_p.append(torch.zeros(n_batch,self.z_size[-1]))
+            z_var_p.append(torch.ones(n_batch, self.z_size[-1]))
+            if z is not None:
+                z_sample.append(z)
+            else:
+                z_sample.append(self.sample_z(z_mu_p[0], z_var_p[0]))
+            _,_,z_sample = self.top_down(z_sample,z_mu_p,z_var_p)
+            z = z_sample[0].unsqueeze(1) # n_batch * 1 * ladder_z_size[0]
+
+            # inital values
+            h = self.decoder.map_z2hc(z_sample[0]) # n_batch * dec_hid_sz *2
+            h_0, c_0 = h[:,:self.decoder.d_d_h], h[:,self.decoder.d_d_h:] # n_batch * dec_hid_sz
+            h_0 = h_0.unsqueeze(0).repeat(self.decoder.lstm.num_layers, 1, 1)  # dec_n_layer * n_batch * dec_hid_sz
+            c_0 = c_0.unsqueeze(0).repeat(self.decoder.lstm.num_layers, 1, 1)  # dec_n_layer * n_batch * dec_hid_sz
+            w = torch.tensor(self.bos).repeat(n_batch) # n_batch
+            x = torch.tensor([self.pad]).repeat(n_batch,max_len) # n_batch * max_len
+
+            x[:, 0] = self.bos
+            end_pads = torch.tensor([max_len]).repeat(n_batch) # a tensor record the length of each molecule, size=n_batch
+            eos_mask = torch.zeros(n_batch, dtype=torch.bool) # a tensor indicate the molecular generation process is over or not,size=n_batch
+
+            # generating cycle
+            for i in range(1,max_len):
+                x_emb = self.embedding(w).unsqueeze(1) # n_batch * 1 * embed_size
+                x_input = torch.cat([x_emb, z], dim=-1) # n_batch * 1 * (embed_size + ladder_z_size[0])
+                output, (h_0,c_0) = self.decoder.lstm(x_input, (h_0,c_0)) # output size : n_batch * 1 * dec_hid_sz
+                y = self.decoder.decoder_fc(output.squeeze(1))
+                y = F.softmax(y / temp, dim=-1) # n_batch * n_vocab
+
+                w = torch.multinomial(y, 1)[:, 0] # input of next generate step, size=n_batch
+                x[~eos_mask, i] = w[~eos_mask] # add generated atom to molecule
+                i_eos_mask = ~eos_mask & (w == self.eos)
+                end_pads[i_eos_mask] = i + 1 # update end_pads
+                eos_mask = eos_mask | i_eos_mask #update eos_mask
+
+            # Converting `x` to list of tensors
+            new_x = []
+            for i in range(x.size(0)):
+                new_x.append(x[i, :end_pads[i]])
+            print(x,new_x)
+            return [self.tensor2string(i_x) for i_x in new_x]
 
 
 class MLP(torch.nn.Module):
@@ -141,46 +226,4 @@ class MLP(torch.nn.Module):
         var = F.softplus(self.var(layer2)) + 1e-8
         return layer2,mu,var
 
-
-if __name__ == '__main__':
-    class config():
-        def __init__(self,emb_sz,dec_hid_sz,dec_n_layer,dropout,embed_size,enc_hidden_size,enc_num_layers,
-                     enc_sorted_seq,enc_bidirectional,enc_dropout,enc_type,dec_type,LVAE_d_size,LVAE_z_size,
-                     LVAE_z2z_layer_size,LVAE_batch_size):
-            self.emb_sz = emb_sz
-            self.dec_hid_sz = dec_hid_sz
-            self.dec_n_layer = dec_n_layer
-            self.dropout = dropout
-            self.embed_size = embed_size
-            self.enc_hidden_size = enc_hidden_size
-            self.enc_num_layers = enc_num_layers
-            self.enc_sorted_seq = enc_sorted_seq
-            self.enc_bidirectional = enc_bidirectional
-            self.enc_dropout = enc_dropout
-            self.enc_type = enc_type
-            self.dec_type = dec_type
-            self.LVAE_d_size = LVAE_d_size
-            self.LVAE_z_size = LVAE_z_size
-            self.LVAE_z2z_layer_size = LVAE_z2z_layer_size
-            self.LVAE_batch_size = LVAE_batch_size
-
-
-    test_split = DatasetSplit("test", r"C:\Users\ASUS\github\MolLVAE\MolLVAE\data\test.csv")
-    test_dataloader = test_split.get_dataloader(batch_size=512)
-    for i,batch in enumerate(test_dataloader):
-        if i == 0:
-            input = batch
-        else:
-            break
-    d_size = [512,256,128,64,32]
-    z_size = [64,32,16,8,4]
-    z2z_layer_size = [8,16,32,64]
-    batch_size = 512
-    vocab = test_split._vocab
-    config = config(emb_sz=10,dec_hid_sz=5,dec_n_layer=2,dropout=0,embed_size=10,enc_hidden_size=5,enc_num_layers=2,
-                    enc_sorted_seq=True,enc_bidirectional=False,enc_dropout=0,enc_type='lstm',dec_type='lstm',LVAE_d_size=d_size,LVAE_z_size=z_size,
-                    LVAE_z2z_layer_size=z2z_layer_size,LVAE_batch_size=batch_size)
-    model = LVAE(vocab,config)
-    kl_loss,recon_loss = model(input)
-    print(kl_loss,recon_loss)
 
